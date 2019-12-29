9cfcebde09261f6865332e8704e261189840d9fb
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-28 21:02:56 -0300

    anotation for date and time

--- lib/src/annotation.dart
+++ lib/src/annotation.dart
@@ -6,3 +6,11 @@ class Field {
   final String name;
   const Field({this.name});
 }
+
+class Date extends Field {
+  const Date({String name}) : super(name: name);
+}
+
+class Time extends Field {
+  const Time({String name}): super(name: name);
+}
