710c4644b24aebf985d57afe9a45e95872c3ee85
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-31 14:59:00 -0300

    annotation for generated of the field many to one

--- lib/src/annotation.dart
+++ lib/src/annotation.dart
@@ -12,5 +12,14 @@ class Date extends Field {
 }
 
 class Time extends Field {
-  const Time({String name}): super(name: name);
+  const Time({String name}) : super(name: name);
+}
+
+class ManyToOne extends Field {
+  final String displayField;
+  const ManyToOne(String this.displayField, {String name}) : super(name: name);
+}
+
+class OneToMany extends Field {
+  const OneToMany(String displayField, {String name}) : super(name: name);
 }
